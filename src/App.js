8828a8c6cf6008557b851c6976057b20012a2c15
MOgorodnik <mr.ogorodnik@gmail.com>
2020-08-13 23:21:34 +0300

    Im need understand wich way is more correct for import different library

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
 import React from 'react';
 
-// https://chriscourses.com/blog/loading-fonts-webpack
-// https://www.codegrepper.com/code-examples/javascript/add+image+in+react+
-
-// how import js 4 ways
-// https://medium.com/better-programming/4-ways-of-adding-external-js-files-in-reactjs-823f85de3668
-
-import './fonts/ionicons.eot';
-import './fonts/ionicons.svg';
-import './fonts/ionicons.ttf';
-import './fonts/ionicons.woff';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'jquery/src/jquery';
-// import 'bootstrap/dist/js/bootstrap.min.js';
-// import '../node_modules/bootstrap/dist/js/bootstrap.min';
-
 import './App.css';
 import Header from './components/header/Header'
 import Navbar from './components/navbar/Navbar'
@@ -28,19 +13,6 @@ function App() {
             <Header />
             <Header />
             <Navbar />
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
         </div>
         </>
     );
--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ScriptTag from 'react-script-tag';
+// import ScriptTag from 'react-script-tag';
+
+// https://chriscourses.com/blog/loading-fonts-webpack
+// https://www.codegrepper.com/code-examples/javascript/add+image+in+react+
+
+// how import js 4 ways
+// https://medium.com/better-programming/4-ways-of-adding-external-js-files-in-reactjs-823f85de3668
+
+import './fonts/ionicons.eot';
+import './fonts/ionicons.svg';
+import './fonts/ionicons.ttf';
+import './fonts/ionicons.woff';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.min.js';
+import $ from "jquery";
+import 'jquery/src/jquery';
+import 'bootstrap/dist/js/bootstrap';
 
 import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-const Demo = props => (
-    <>
-    <ScriptTag type="text/javascript" src="./scripts/jquery-3.1.1.min.js" />
-    <ScriptTag type="text/javascript" src="./scripts/bootstrap.min.js" />
-    <ScriptTag type="text/javascript" src="./scripts/script.js" />
-    </>
-);
-console.log(Demo);
+// const Demo = props => (
+//     <>
+//     <ScriptTag type="text/javascript" src="./scripts/jquery-3.1.1.min.js" />
+//     <ScriptTag type="text/javascript" src="./scripts/bootstrap.min.js" />
+//     <ScriptTag type="text/javascript" src="./scripts/script.js" />
+//     </>
+// );
+// console.log(Demo);
+console.log($);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Demo />
+    {/*<Demo />*/}
   </React.StrictMode>,
   document.getElementById('root')
 );
