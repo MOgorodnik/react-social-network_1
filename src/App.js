203d26798b7b76e3b993d4a035cb063665bf5e41
MOgorodnik <mr.ogorodnik@gmail.com>
2020-08-27 22:10:27 +0300

    Add Navbar Navbar NavItem NavDropdown MenuItem

--- src/App.js
+++ src/App.js
@@ -5,37 +5,63 @@ import Alert from "react-bootstrap/lib/Alert";
 import Button from "react-bootstrap/lib/Button";
 
 // import our fonts
-import './fonts/ionicons.eot';
-import './fonts/ionicons.svg';
-import './fonts/ionicons.ttf';
-import './fonts/ionicons.woff';
+import "./fonts/ionicons.eot";
+import "./fonts/ionicons.svg";
+import "./fonts/ionicons.ttf";
+import "./fonts/ionicons.woff";
 
-import './App.css';
-import Header from './components/header/Header'
-import Navbar from './components/navbar/Navbar'
+import "./App.css";
+import Header from "./components/header/Header";
+import NavbarSite from "./components/navbar/NavbarSite";
+// import Nav from "./components/navbar/Nav";
+// import NavItem from "./components/navbar/NavItem";
+// import NavDropdown from "./components/navbar/NavDropdown";
+// import MenuItem from "./components/navbar/MenuItem";
 // import Preloader from './components/preloader1/Preloader'
 
 function App(props) {
-    console.log(props);
-    const {hideLoader, showLoader} = props;
-    useEffect(hideLoader, []);
+  console.log(props);
+  const { hideLoader, showLoader } = props;
+  useEffect(hideLoader, []);
 
-    return (
-        <>
-            <div className="App">
-                <Header />
-                <Header />
-                <Navbar />
-                11
-                <Alert bsStyle="warning">
-                    <strong>Holy guacamole!</strong>
-                    Best check yo self, you're not looking too good.
-                </Alert>
-                11
-            </div>
-            {/*<Preloader />*/}
-        </>
-    );
+  return (
+    <>
+      <div className="App">
+        <Header />
+        <hr />
+        <NavbarSite />
+        {/* <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">React-Bootstrap</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">
+              Link
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Link
+            </NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.4}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar> */}
+        ; 11
+        <Alert bsStyle="warning">
+          <strong>Holy guacamole!</strong>
+          Best check yo self, you're not looking too good.
+        </Alert>
+        11
+      </div>
+      {/*<Preloader />*/}
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,27 +11,25 @@ import './index.css';
 
 import App from './App';
 
-// import * as serviceWorker from './serviceWorker';
 
 const loader = document.querySelector('#spinner-wrapper');
 console.log(loader);
 
+
 // if you want to show the loader when React loads data again
-const showLoader = () => loader.setAttribute('hidden', false);
+// const showLoader = () => loader.setAttribute('hidden', false);
 
 const hideLoader = () => loader.setAttribute('hidden', true);
+
 
 // the setTimeout simulates the time it takes react to load, and is not part of the solution
 setTimeout(() =>
     // the show/hid
     ReactDOM.render(
         <React.StrictMode>
-            <App hideLoader={hideLoader} showLoader={showLoader} />
+            {/* <App hideLoader={hideLoader} showLoader={showLoader} /> */}
+            <App hideLoader={hideLoader} />
         </React.StrictMode>,
         document.getElementById('root')
-    ), 1000);
+    ), 200);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
